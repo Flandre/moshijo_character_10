cb75b379281dd9dff22d2e840a29ee2ac576a740
shikong <a26214311@gmail.com>
2017-02-07 17:25:23 +0800

    f

--- test4.js
+++ test4.js
@@ -1,10 +1,12 @@
 var outvalue = [];
 var body = "";
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
 process.stdin.on('data', function (chunk) {
-  body+=chunk.toString();
-});
-process.stdin.on('end', function () {
-  var bodya = body.split("\n");
+
+  var bodya = chunk.split("\n");
   var n = parseInt(bodya[0]);
   var costa = bodya[1].split(" ");
   var costpast = parseInt(costa[0]);
@@ -13,30 +15,38 @@ process.stdin.on('end', function () {
   var ststr = bodya[2];
   var st = [];
   for(var i=0;i<n;i++){
-    if(st.charAt(i)+""=="s"){
+    if(ststr.charAt(i)+""=="s"){
       st[i]=1;
     }else{
       st[i]=0;
     }
   }
+  console.log(st);
   queue.push([[n],{},0,st]);
   while(queue.length>0){
-    var now = queue[0];
-    var his = now[0];
-    var map = now[1];
-    var oldcost = now[2]
-    var nowst = now[3];
+
     for(var i=0;i<n;i++){
+      var now = queue[0];
+      var his = now[0];
+      var map = now[1];
+      var oldcost = now[2];
+      var nowst = now[3];
+
       if(nowst[i]==1){
         var lastpoint = his[his.length-1];
         if(lastpoint != i){
           map[i]=1;
-          his.push(i);
+          var newhis = [];
+          for(var j=0;j<his.length;j++){
+            newhis.push(his[j]);
+          }
+          newhis.push(i);
+
           var newst = []
           for(var j=0;j<i;j++){
             newst.push(nowst[i]);
           }
-          for(var j=i;i<n;j++){
+          for(var j=i;j<n;j++){
             newst.push(1-nowst[i]);
           }
           var newcost = oldcost;
@@ -45,11 +55,12 @@ process.stdin.on('end', function () {
           }else{
             newcost = newcost = costpast;
           }
-          var newpoint = [his,map,newcost,newst];
+          var newpoint = [newhis,map,newcost,newst];
           console.log(newpoint);
         }
       }
     }
+    queue = queue.slice(1);
   }
 });
 
