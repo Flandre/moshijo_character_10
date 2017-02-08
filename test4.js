1c7469a1c6c00e4ad062b2008394e6444b925bf2
shikong <a26214311@gmail.com>
2017-02-08 11:01:15 +0800

    fix

--- test4.js
+++ test4.js
@@ -5,12 +5,15 @@ process.stdin.setEncoding('utf8');
 // 自分の得意な言語で
 // Let's チャレンジ！！
 process.stdin.on('data', function (chunk) {
+  body += chunk;
+});
 
-  var bodya = chunk.split("\n");
+process.stdin.on('end', function () {
+  var bodya = body.split("\n");
   var n = parseInt(bodya[0]);
   var costa = bodya[1].split(" ");
-  var costpast = parseInt(costa[0]);
-  var costfuture = parseInt(costa[1]);
+  var costfuture = parseInt(costa[0]);
+  var costpast= parseInt(costa[1]);
   var queue = [];
   var ststr = bodya[2];
   var st = [];
@@ -21,12 +24,14 @@ process.stdin.on('data', function (chunk) {
       st[i]=0;
     }
   }
-  console.log(st);
-  queue.push([[n],{},0,st]);
+  queue.push([[n-1],{},0,st]);
+  var mincost = 999999;
+  var route = [];
+  var flag = false;
   while(queue.length>0){
-
+    var now = queue[0];
+    queue = queue.slice(1);
     for(var i=0;i<n;i++){
-      var now = queue[0];
       var his = now[0];
       var map = now[1];
       var oldcost = now[2];
@@ -35,7 +40,6 @@ process.stdin.on('data', function (chunk) {
       if(nowst[i]==1){
         var lastpoint = his[his.length-1];
         if(lastpoint != i){
-          map[i]=1;
           var newhis = [];
           for(var j=0;j<his.length;j++){
             newhis.push(his[j]);
@@ -43,25 +47,83 @@ process.stdin.on('data', function (chunk) {
           newhis.push(i);
 
           var newst = []
-          for(var j=0;j<i;j++){
-            newst.push(nowst[i]);
+          for(var j=0;j<=i;j++){
+            newst.push(nowst[j]);
           }
-          for(var j=i;j<n;j++){
-            newst.push(1-nowst[i]);
+          for(var j=i+1;j<n;j++){
+            newst.push(1-nowst[j]);
           }
           var newcost = oldcost;
           if(lastpoint < i){
             newcost = newcost + costfuture;
           }else{
-            newcost = newcost = costpast;
+            newcost = newcost + costpast;
           }
-          var newpoint = [newhis,map,newcost,newst];
-          console.log(newpoint);
+          var newpoint = [newhis,{},newcost,newst];
+          var finish = true;
+          for(var j=0;j<n;j++){
+            if(newhis.indexOf(j)==-1){
+              finish = false;
+              break;
+            }
+          }
+          if(finish){
+            flag = true;
+            if(newst[n-1]==0){
+              if(i!=0){
+                var totalcost = newcost + costpast + costfuture;
+                if(totalcost<mincost){
+                  mincost = totalcost;
+                  route = newhis;
+                  route.push(0);
+                  route.push(n-1);
+                }
+              }else{
+                var can = false;
+                for(var j=0;j<n;j++){
+                  if(newst[j]==1){
+                    can = true;
+                    totalcost = newcost + costfuture + costfuture;
+                    if(totalcost<mincost){
+                      mincost = totalcost;
+                      route = newhis;
+                      route.push(j);
+                      route.push(n-1);
+                    }
+                    break;
+                  }
+                }
+                if(can = false){
+                  flag = false;
+                }
+              }
+            }else{
+              totalcost = newcost + costfuture;
+              if(totalcost<mincost){
+                mincost = totalcost;
+                route = newhis;
+                route.push(n-1);
+              }
+            }
+          }else{
+            if(flag == false){
+              queue.push(newpoint);
+            }
+          }
         }
       }
     }
-    queue = queue.slice(1);
+    queue.sort(function(a,b){return a[2]-b[2]});
   }
+  var output = "";
+  for(var i=0;i<route.length;i++){
+    if(output==""){
+      output = output + (route[i]+1);
+    }else{
+      output = output + " " + (route[i]+1);
+    }
+  }
+  console.log(output);
 });
 
 
