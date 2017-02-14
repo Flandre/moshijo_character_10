00d9f3682816e955609392140ffa73bb00d708ef
shikong <a26214311@gmail.com>
2017-02-14 11:26:50 +0800

    f

--- a009s.js
+++ a009s.js
@@ -6,94 +6,96 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', function (chunk) {
   body += chunk;
 });
-process.stdin.on('end', function () {
+process.stdin.on('end', function (chunk) {
   var linea = body.split("\n");
   var fl = linea[0];
   var fla = fl.split(" ");
-  var width = parseInt(fla[0]);
-  var height = parseInt(fla[1]);
+  var width = parseInt(fla[1]);
+  var height = parseInt(fla[0]);
   var maze = [];
   var value = [];
-  for(var i=0;i<height;i++){
+  for(var i=-1;i<height+1;i++){
     maze[i]=[];
     value[i] = [];
   }
   var start = [];
   var goal = [];
   for(var i=0;i<height;i++){
-    var mazel = linea[i+1];
-    maze[i]=mazel.split(" ");
     for(var j=0;j<width;j++){
-      if(maze[i][j]=="s"){
-        start = [i,j];
-      }
-      if(maze[i][j]=="g"){
-        goal = [i,j];
-      }
-      value[i][j]=993999;
+      maze[i][j]=linea[i+1].charAt(j)+"";
     }
   }
-  var taskmap = {};
-  var task = [];
-  function getnext(x, y) {
-    var rett = [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]];
-    var ret = [];
-    for (var i = 0; i < rett.length; i++) {
-      if (maze[rett[i][0]]) {
-        var tmp = maze[rett[i][0]][rett[i][1]];
-        if (tmp == "1" || tmp == undefined) {
+  cal();
+  function cal(){
+    var count=0;
+    var x=-1;
+    var y=0;
+    var direction = "right";
+    while(true){
+      if(count>0&&(x<0||x>width-1||y<0||y>height-1)){
+        break;
+      }
+      var nx;
+      var ny;
+      var nd;
 
-        } else {
-          ret.push(rett[i]);
+      if(direction=="right"){
+        nx = x + 1;
+        ny = y;
+        var road = maze[ny][nx];
+        if(road == "_"){
+          nd = direction;
+        }else if(road == "\\"){
+          nd = "down";
+        }else if(road == "/"){
+          nd = "up";
+        }
+      }else if(direction=="up"){
+        nx = x;
+        ny = y-1;
+        var road = maze[ny][nx];
+        if(road == "_"){
+          nd = direction;
+        }else if(road == "\\"){
+          nd = "left";
+        }else if(road == "/"){
+          nd = "right";
+        }
+      }else if(direction=="left"){
+        nx = x - 1;
+        ny = y;
+        var road = maze[ny][nx];
+        if(road == "_"){
+          nd = direction;
+        }else if(road == "\\"){
+          nd = "up";
+        }else if(road == "/"){
+          nd = "down";
+        }
+      }else if(direction=="down"){
+        nx = x;
+        ny = y+1;
+        var road = maze[ny][nx];
+        if(road == "_"){
+          nd = direction;
+        }else if(road == "\\"){
+          nd = "right";
+        }else if(road == "/"){
+          nd = "left";
         }
       }
+
+      count = count + 1;
+      x = nx;
+      y = ny;
+      direction = nd;
+
+
     }
-    return ret;
+    console.log(count-1);
   }
 
-  function search(x, y) {
-    var next = getnext(x, y);
-    for (var i = 0; i < next.length; i++) {
-      var nx = next[i][0];
-      var ny = next[i][1];
-      var mv = maze[nx][ny];
-      var ovalue = value[nx][ny];
-      var nvalue = value[x][y];
-      var nowcost = nvalue;
-      var cost = 1;
-      var tcost = nowcost + cost;
-      if (tcost < ovalue) {
-        value[nx][ny]=tcost;
-        var newtaskno = (nx << 15) + ny;
-        var queuedtask = taskmap[newtaskno];
-        if (queuedtask == undefined) {
-          task.push(newtaskno);
-        }
-      }
-    }
-  }
 
-  function dotask(){
-    var newtaskno = (start[0] << 15) + start[1];
-    value[start[0]][start[1]]=0;
-    task.push(newtaskno);
-    while (task.length != 0) {
-      var nowtask = task[0];
-      task = task.slice(1);
-      var ny = nowtask & 32767;
-      var nx = nowtask >> 15;
-      delete(taskmap[nowtask]);
-      search(nx, ny);
-    }
-    if(value[goal[0]][goal[1]]==993999){
-      console.log("Fail");
-    }else{
-      console.log(value[goal[0]][goal[1]])
-    }
-
-  }
-
-  dotask();
 
 });
 
